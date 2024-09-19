d816a01b00709b86119b1f47202ff152e1b0c1b4
Guilhermemrqsm <gm81323905@gmail.com>
2024-09-18 21:32:55 -0300

    Media

---
+++ media.py
@@ -0,0 +1,9 @@
+num1 = int(input("Informe o valor 1 :"))
+num2 = int(input("Informe o valor 2 :"))
+num3 = int(input("Informe o valor 3 :"))
+num4 = int(input("Informe o valor 4 :"))
+
+soma = (num1 + num2 + num3 + num4)
+media = soma /4
+
+print("media = "+str (media)) 
